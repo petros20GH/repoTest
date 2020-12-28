e821aabc8be08f68e2483dc3a729c00472fab85b
Peter Panaioti <ppanaioti@gmail.com>
2020-12-28 14:20:37 -0700

    Updated C++ file and added a new Python script

---
+++ newFile.py
@@ -0,0 +1,3 @@
+## New Python Test File
+print("This is a python test file.")
+print("Area of a circle = pi * r * r")
--- tester.cpp
+++ tester.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
 int main() {
+    char c;
+
     std::cout << "Hello World!" << std::endl;
-    
+
+    std::cout << "Enter any key to quit: ";
+    std::cin >> c;
+
     return 0;
 }
     
